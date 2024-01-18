f740a9e872d304e83a3666a072143c1f25195bbc
Ashearerclan <aydenshearer@icloud.com>
2024-01-18 14:31:59 -0700

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess > number)
+            {
+               Console.Write("Lower");
+                Console.ReadLine(); 
+            }
+
+            else if (guess < number)
+            {
+               Console.Write("Higher");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }   
 }
